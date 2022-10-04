c35d70294f0481744b54a468cd6e9f1cf5885f02
yeony612 <77176550+yeony612@users.noreply.github.com>
2022-10-04 14:04:23 -0400

    updated some styles and names

--- pages/components/CampaignInputs.jsx
+++ pages/components/CampaignInputs.jsx
@@ -2,15 +2,14 @@ import { InputGroup, InputText, Section } from "pier-design-system";
 import { useContext, useState } from "react";
 import { InputsContext } from "../Contexts/InputsContext";
 
-const CampaignInfo = (props) => {
-
+const CampaignInputs = (props) => {
   // const { setClientName } = useContext(InputsContext);
   // const [clientName, setClientName] = useState("");
 
   const handleChange = (event, field) => {
     const value = event.target.value;
-    props.changeHandler(value,field);
-  }
+    props.changeHandler(value, field);
+  };
 
   return (
     <Section padding="sm" className="client-section">
@@ -20,31 +19,33 @@ const CampaignInfo = (props) => {
             size="sm"
             placeholder="Enter Text"
             data-field="clientName"
-            onChange={ (event) => { handleChange(event, 'clientName')}}
+            onChange={(event) => {
+              handleChange(event, "clientName");
+            }}
           />
         </InputGroup>
       </div>
 
       <div className="campaign-name-input">
         <InputGroup label="campaign name" size="sm" error="" dark={false}>
-          <InputText size="sm"
+          <InputText
+            size="sm"
             placeholder="Enter Text"
-            onChange={ (event) => { handleChange(event, 'campaignName')}}
+            onChange={(event) => {
+              handleChange(event, "campaignName");
+            }}
           />
         </InputGroup>
       </div>
 
       <div className="campaign-budget-input">
-        <InputGroup
-          label="budget"
-          size="sm"
-          error="(optional)"
-          dark={false}
-        >
+        <InputGroup label="budget" size="sm" error="(optional)" dark={false}>
           <InputText
             size="sm"
             placeholder="Enter Number"
-            onChange={ (event) => { handleChange(event, 'campaignBudget')}}
+            onChange={(event) => {
+              handleChange(event, "campaignBudget");
+            }}
             // onChange={handleChange}
             // value={inputValue}
           />
@@ -54,4 +55,4 @@ const CampaignInfo = (props) => {
   );
 };
 
-export default CampaignInfo;
+export default CampaignInputs;
