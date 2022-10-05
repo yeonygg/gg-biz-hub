4624263faa0f247cf22995d0186d78f40c1f9536
yeony612 <77176550+yeony612@users.noreply.github.com>
2022-10-05 13:48:31 -0400

    added unittypes to master list of unit types

--- pages/constants/constants.js
+++ pages/constants/constants.js
@@ -1,12 +1,65 @@
 const unitTypes = [
-    {
-        id: 0,
-        name: "In-Screen",
-        abbrev: "IS",
-        minSpend: 25000,
-        highImpact: false,
-        isCustom: false,
-    }
+  {
+    id: 0,
+    name: "In-Screen",
+    abbrev: "IS",
+    minSpend: 25000,
+    highImpact: false,
+    isCustom: false,
+    turnaroundTime: 2,
+    cesRequired: false,
+  },
+
+  {
+    id: 1,
+    name: "In-Image",
+    abbrev: "II",
+    minSpend: 25000,
+    highImpact: false,
+    isCustom: false,
+    turnaroundTime: 2,
+    cesRequired: false,
+  },
+  {
+    id: 2,
+    name: "In-Screen Expandable Video",
+    abbrev: "ISXV",
+    minSpend: 25000,
+    highImpact: false,
+    isCustom: false,
+    turnaroundTime: 2,
+    cesRequired: true,
+  },
+  {
+    id: 3,
+    name: "In-Image Expandable Video",
+    abbrev: "IIXV",
+    minSpend: 25000,
+    highImpact: false,
+    isCustom: false,
+    turnaroundTime: 2,
+    cesRequired: true,
+  },
+  {
+    id: 4,
+    name: "6-Sec Canvas Video",
+    abbrev: "6sCNV",
+    minSpend: 25000,
+    highImpact: false,
+    isCustom: false,
+    turnaroundTime: 2,
+    cesRequired: false,
+  },
+  {
+    id: 5,
+    name: "GG Standard Display",
+    abbrev: "6sCNV",
+    minSpend: 25000,
+    highImpact: false,
+    isCustom: false,
+    turnaroundTime: 2,
+    cesRequired: true,
+  },
 ];
 
-export {unitTypes};
+export { unitTypes };
