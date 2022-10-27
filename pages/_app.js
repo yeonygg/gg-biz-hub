ae95a19bc3204e2f1af8f47cd14394a79482e8a1
Dan McKenna <dmckenna@gumgum.com>
2022-10-27 15:26:10 -0700

    CSS cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import "../node_modules/pier-design-system/dist/styles.css"
+import "pier-design-system/dist/styles.css";
 import "../styles/styles.css";
-// import "../styles/pier.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
