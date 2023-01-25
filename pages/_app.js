a9be478ed73ed7b82fda96d95d44ed978a250557
yeony612 <77176550+yeony612@users.noreply.github.com>
2023-01-25 17:34:28 -0500

    fixed rebase issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 import "pier-design-system/dist/styles.css";
 import "../styles/styles.css";
 import "../styles/globals.css";
+=======
+import styles from "../styles/styles.css";
+import styles from "../styles/globals.css";
+import "pier-design-system/dist/styles.css";
+>>>>>>> 6888cfb (updated styles)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
