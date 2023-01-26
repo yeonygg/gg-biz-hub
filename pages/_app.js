40c2299be686cf8c62bee27226f7e8311d51c3fe
yeony612 <77176550+yeony612@users.noreply.github.com>
2023-01-26 10:54:51 -0500

    resolved app issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import styles from "../styles/styles.css";
-import styles from "../styles/globals.css";
->>>>>>> 6888cfb (updated styles)
 import "pier-design-system/dist/styles.css";
 import "../styles/styles.css";
-=======
-import "../node_modules/pier-design-system/dist/styles.css"
-import "../styles/styles.css";
-// import "../styles/pier.css";
->>>>>>> aa4e963 (updated css order)
-=======
-import "pier-design-system/dist/styles.css";
-import "../styles/styles.css";
->>>>>>> ae95a19 (CSS cleanup)
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
