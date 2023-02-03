b93781a305cff2ac3f75eac8f31c4e36cb9b2313
yeony612 <77176550+yeony612@users.noreply.github.com>
2023-02-03 17:02:23 -0500

    resolved app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "pier-design-system/dist/styles.css";
 import "../styles/styles.css";
 =======
@@ -6,6 +7,10 @@ import "../node_modules/pier-design-system/dist/styles.css"
 import "../styles/styles.css";
 // import "../styles/pier.css";
 >>>>>>> aa4e963 (updated css order)
+=======
+import "pier-design-system/dist/styles.css";
+import "../styles/styles.css";
+>>>>>>> ae95a19 (CSS cleanup)
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
