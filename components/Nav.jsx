0d0cd7226c6f575be00e33efaadbd6521ab3edbf
yeony612 <77176550+yeony612@users.noreply.github.com>
2023-03-10 11:30:14 -0500

    fixed nav styles

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,61 +1,78 @@
 import React from "react";
 import { useState, useEffect, Component } from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import Image from "next/image";
 
 function Nav() {
-    return (
-        <div className="pier-nav pier-nav--dark">
-            <div className="pier-nav__tab pier-nav__tab--dark">
-                <button className="pier-button-icon pier-button-icon--xs pier-button-icon--secondary"><i className="pier-button-icon__icon fas fa-chevron-left"></i></button>
-            </div>
-            <h3 className="pier-heading pier-heading--sm">
-                Navigation
-            </h3>
-            <div className="pier-nav__item-group">
-                <div className="pier-nav__item-container">
-                    <span className="pier-nav__item-dropdown-toggle
-                            pier-nav__item-dropdown-toggle--toggled"></span>
-                    <div className="pier-nav__item">
-                        <span className="pier-nav__item-icon fas fa-ruler-triangle"></span>Business Rules
-                    </div>
-                    <div className="pier-nav__item-dropdown">
-                        <NavLink to="/custom-creatives">
-                            <div className="pier-nav__sub-item">
-                                <div className="pier-nav__sub-item-line"></div>
-                                Custom Creative Guidelines
-                            </div>
-                        </NavLink>
-                        <NavLink to="/creative-rules">
-                            <div className="pier-nav__sub-item">
-                                <div className="pier-nav__sub-item-line"></div>
-                                Creative Rules
-                            </div>
-                        </NavLink>
-                        <NavLink to="/rate-card">
-                            <div className="pier-nav__sub-item">
-                                <div className="pier-nav__sub-item-line"></div>
-                                Rate Card
-                            </div>
-                        </NavLink>
-                        <NavLink to="/studies">
-                            <div className="pier-nav__sub-item">
-                                <div className="pier-nav__sub-item-line"></div>
-                                Research & Data
-                            </div>
-                        </NavLink>
-                    </div>
-                </div>
-                <div className="pier-nav__item-container">
-                    <NavLink to="/generator">
-                        <div className="pier-nav__item">
-                            <span className="pier-nav__item-icon fas fa-cog"></span>
-                            SLA Generator
-                        </div>
-                    </NavLink>
-                </div>
-            </div>
+  return (
+    <div className="pier-nav pier-nav--dark">
+      <div
+        className="-d-flex -justify-content-center -m-b-4"
+        style={{ width: "100%" }}
+      >
+        {" "}
+        <Image
+          alt="gumgum logo"
+          src="/gumgum_logo_dark.svg"
+          width="150%"
+          height="100%"
+          key="1"
+          style={{ verticalAlign: "middle" }}
+        />
+      </div>
+      <div className="pier-nav__tab pier-nav__tab--dark">
+        <button className="pier-button-icon pier-button-icon--xs pier-button-icon--secondary">
+          <i className="pier-button-icon__icon fas fa-chevron-left"></i>
+        </button>
+      </div>
+      <div className="pier-nav__item-group">
+        <div className="pier-nav__item-container">
+          <span
+            className="pier-nav__item-dropdown-toggle pier-nav__item-dropdown-toggle--dark
+                            pier-nav__item-dropdown-toggle--toggled"
+          ></span>
+          <div className="pier-nav__item pier-nav__item--dark">
+            <span className="pier-nav__item-icon fas fa-ruler-triangle"></span>
+            Business Rules
+          </div>
+          <div className="pier-nav__item-dropdown">
+            <NavLink to="/custom-creatives">
+              <div className="pier-nav__sub-item pier-nav__sub-item--dark">
+                <div className="pier-nav__sub-item-line"></div>
+                Custom Creative Guidelines
+              </div>
+            </NavLink>
+            <NavLink to="/creative-rules">
+              <div className="pier-nav__sub-item pier-nav__sub-item--dark">
+                <div className="pier-nav__sub-item-line"></div>
+                Creative Rules
+              </div>
+            </NavLink>
+            <NavLink to="/rate-card">
+              <div className="pier-nav__sub-item pier-nav__sub-item--dark">
+                <div className="pier-nav__sub-item-line"></div>
+                Rate Card
+              </div>
+            </NavLink>
+            <NavLink to="/studies">
+              <div className="pier-nav__sub-item pier-nav__sub-item--dark">
+                <div className="pier-nav__sub-item-line"></div>
+                Research & Data
+              </div>
+            </NavLink>
+          </div>
         </div>
-    )
-};
+        <div className="pier-nav__item-container">
+          <NavLink to="/generator">
+            <div className="pier-nav__item pier-nav__item--dark">
+              <span className="pier-nav__item-icon fas fa-cog"></span>
+              SLA Generator
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Nav;
