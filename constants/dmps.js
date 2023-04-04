a7a3212b31046baa20f3932102a8e41c33273281
Daniel McKenna <dmckenna@gumgum.com>
2023-04-04 11:41:17 -0700

    fixed dmp issue

--- constants/dmps.js
+++ constants/dmps.js
@@ -3,6 +3,7 @@ var dmp = [
   {
     id: 0,
     dataType: "GumGum First Part (Contextual)",
+    cpmUpcharge: {0: 0, 25000: 0, 50000: 0, 75000: 0, 100000: 0, 125000: 0, 200000: 0, 300000: 0},
     minSpend: 25000,
     info: [
       { spend: "$25k-$200k+", locked: "Unlocked", cpmUpcharge: 0 },
@@ -12,6 +13,7 @@ var dmp = [
   {
     id: 1,
     dataType: "Lotame LDX ($0.20)",
+    cpmUpcharge: {0: 0.2, 25000: 0.2, 50000: 0.2, 75000: 0.2, 100000: 0, 125000: 0, 200000: 0, 300000: 0},
     info: [
       { spend: "$25k-$99k", locked: "Unlocked", cpmUpcharge: 0.2 },
       { spend: "$100k-$300k+", locked: "Unlocked", cpmUpcharge: 0 },
@@ -22,6 +24,7 @@ var dmp = [
   {
     id: 2,
     dataType: "3rd Party Branded Data",
+    cpmUpcharge: {0: 1, 25000: 1, 50000: 1, 75000: 1, 100000: 0.5, 125000: 0.5, 200000: 0, 300000: 0},
     info: [
       { spend: "$25k-$99k", locked: "Unlocked", cpmUpcharge: 1 },
       { spend: "$100k-$199k", locked: "Unlocked", cpmUpcharge: 0.5 },
@@ -33,6 +36,7 @@ var dmp = [
   {
     id: 3,
     dataType: "Client Ad Serving Fee's (DCM, IAS, DV, Other)",
+    cpmUpcharge: {0: 0.15, 25000: 0.15, 50000: 0.15, 75000: 0.15, 100000: 0, 125000: 0, 200000: 0, 300000: 0},
     info: [
       { spend: "$25k-$99k", locked: "Unlocked", cpmUpcharge: 0.15 },
       { spend: "$100k-$300k+", locked: "Unlocked", cpmUpcharge: 0 },
@@ -43,6 +47,7 @@ var dmp = [
   {
     id: 4,
     dataType: "Advertiser First Part Data Ingestion",
+    cpmUpcharge: {0: 1, 25000: 1, 50000: 1, 75000: 1, 100000: 1, 125000: 1, 200000: 1, 300000: 0},
     info: [
       { spend: "$25k-$299k", locked: "Unlocked", cpmUpcharge: 1 },
       { spend: "$300k+", locked: "Unlocked", cpmUpcharge: 0 },
@@ -53,6 +58,7 @@ var dmp = [
   {
     id: 5,
     dataType: "ABM Data",
+    cpmUpcharge: {0: 3, 25000: 3, 50000: 3, 75000: 3, 100000: 3, 125000: 3, 200000: 3, 300000: 3},
     info: [
       { spend: "$25k-$99k", locked: "Locked", cpmUpcharge: "N/A" },
       { spend: "$100k-$300k+", locked: "Unlocked", cpmUpcharge: 3 },
